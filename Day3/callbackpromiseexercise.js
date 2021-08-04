e2d3b759eb173c14719e99638e50e36ba575ff71
andyjrlee <andyjrlee@outlook.com>
2021-08-04 12:56:39 +0100

    AL: Pre Lunch commit day 3

--- Day3/callbackpromiseexercise.js
+++ Day3/callbackpromiseexercise.js
@@ -1,18 +1,13 @@
 "use strict"
 
-const greeting = (val) =>  {
+// exercise 1 - my solution
 
-    // Creates an alert(pop-up box) which says hello and appends the value of the variable `name`
-    alert(`The updated value is: ${val+10}`);
+const increase = (num1) =>  {
+  alert(`The updated value is: ${num1+13}`);
   }
-  
-  const processUserInput = (callback) =>  {
-  
-    // A simple user input box appears on the browser that take a value and assigns it to `name`
-    let val = parseInt(prompt('Please enter a number.'));
-  
-    // Then pass the variable `name` to the callback function as parameter
-    callback(val);
+
+  const input = (number) =>  {
+    let value = parseInt(prompt('Please enter a number.'));
+    number(value);
   }
-  //Call the function `processUserInput()` and then pass `greeting` as a parameter.
-  processUserInput(greeting);
+  input(increase);
---
+++ Day3/scopeexercise.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//exercise 1 - my solution
+function hello() {
+    let x = "foo";
+    if (x == "foo") {
+        let y = "bar";
+    }
+    console.log(x);
+    console.log(y);
+}
+hello();
+
+// exercise 2 - my solution
+
+function addUp() {
+    
+}
